b61f42af2ff163cbdcee23ea16991ea130d306ed
alexvile <alexei20293@yahoo.com>
2022-07-10 19:21:35 +0000

    Deploying to gh-pages from @ alexvile/goit-js-hw-11@65c7e39be8e2b82f88d4b7957593026b67987bdc 🚀

--- index.dc9a38d3.js
+++
@@ -1,2 +0,0 @@
-!function(){var o;(o="serbia",fetch("".concat("https://pixabay.com/api","/?key=").concat("28483362-313f04b4986d5508c9cd93d3a","&q=").concat(o,"&image_type=photo&orientation=horizontal&safesearch=true")).then((function(o){return console.log(o),o.json()}))).then((function(o){console.log(o),console.log(o.hits[8].webformatURL)}))}();
-//# sourceMappingURL=index.dc9a38d3.js.map
