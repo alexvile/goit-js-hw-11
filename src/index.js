65c7e39be8e2b82f88d4b7957593026b67987bdc
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-07-10 22:20:57 +0300

    added axios, hbs templates, create image card logic

--- src/index.js
+++ src/index.js
@@ -1,30 +1,106 @@
+import axios from "axios";
+import debounce from "lodash.debounce"
+
+
+import imgCardTpl from './template/imgCardTpl.hbs';
+
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '28483362-313f04b4986d5508c9cd93d3a';
+
+
+const galleryEl = document.querySelector('.gallery');
+const formEl = document.getElementById('search-form');
+const searchEl = formEl.elements["searchQuery"];
+// const submitEl = formEl.elements["submit"];
+
+// console.log(submitEl);
+
 // const FILTER = '?fields=name,capital,population,flags,languages';
 // // const FILTER ='?fields=name.official,capital,population,flags.svg,languages';
 // // const FILTER = '';
 
-let word = 'serbia';
 
-function fetchImages(countryName) {
-    return fetch(`${BASE_URL}/?key=${API_KEY}&q=${countryName}&image_type=photo&orientation=horizontal&safesearch=true`)
-        .then(response => {
-            console.log(response);
-            return response.json();
+
+
+// formEl.addEventListener('input', debounce());
+
+
+// function onInputChange(e) { 
+
+// } 
+
+
+
+axios.get(BASE_URL, {
+    params: {
+        key: API_KEY,
+        q: 'white flower',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true'
+    }
+})
+    .then(function (response) {
+        console.log(response);
+        console.log(response.data);
+        console.log(response.data.hits);
+        const imagesFirst = response.data.hits;
+        // console.log(response.data.hits[12].webformatURL);
+        renderCards(imagesFirst);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
+
+function renderCards(objects) {
+    // console.log(countries);
+    let markup = [];
+
+    objects.map(object => {
+            console.log(object);
+            markup.push(imgCardTpl(object));
         });
+    // refs.countryInfo.innerHTML = markup;
+    
+    galleryEl.innerHTML = markup.join('');
 }
 
 
 
-fetchImages(word).then(pok => {
-    console.log(pok);
-    console.log(pok.hits[8].webformatURL);
-    
-})
 
-// fetch('https://pixabay.com/api/?key=28483362-313f04b4986d5508c9cd93d3a&q=blue+flowers&image_type=photo&pretty=true')
-//     .then(response => { 
-//         return response.json();
-//     }).then(pok => {
-//     console.log(pok);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let word = 'serbia';
+
+
+
+  
+// function fetchImages(image) {
+//     return fetch(`${BASE_URL}/?key=${API_KEY}&q=${image}&image_type=photo&orientation=horizontal&safesearch=true`)
+//         .then(response => {
+//             console.log(response);
+//             return response.json();
+//         });
+// }
+
+
+
+// fetchImages(word).then(data => {
+//     console.log(data);
+//     console.log(data.hits[8].webformatURL);
+    
 // })
+
