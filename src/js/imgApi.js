45884eba0a04f68dd73e139487ae44e05db5bc6e
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-07-25 21:20:31 +0300

    try to fix 301 (2)

--- src/js/imgApi.js
+++ src/js/imgApi.js
@@ -1,30 +1,43 @@
 import axios from "axios";
-const BASE_URL = 'https://pixabay.com/api';
+const BASE_URL = `https://pixabay.com/api`;
 const API_KEY = '28483362-313f04b4986d5508c9cd93d3a';
 
 export default class ImgApi {
     constructor() {
         this.searchQuerry = '';
-        this.page = 1;
+      this.page = 1;
+      this.per_page = 40;
     }
 
   async fetchPictures() {
 // console.log(this);
     
-  const response = await axios.get(BASE_URL, {
-    params: {
-      key: API_KEY,
-      q: `${this.searchQuerry}`,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-      page: `${this.page}`,
-      // per_page:'40'
-      per_page: '40'
-    }
-  });
+  // const response = await axios.get(BASE_URL, {
+  //   params: {
+  //     key: API_KEY,
+  //     q: `${this.searchQuerry}`,
+  //     image_type: 'photo',
+  //     orientation: 'horizontal',
+  //     safesearch: 'true',
+  //     page: `${this.page}`,
+  //     // per_page:'40'
+  //     per_page: '40'
+  //   }
+  // });
     
-  console.log(response);
+  // console.log(response);
+    
+    const options =new URLSearchParams ({
+            key: API_KEY,
+            q: this.searchQuerry,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: 'true',
+            per_page: this.per_page,
+            page: this.page,
+        });
+        const url  = `${BASE_URL}?${options}`;
+        const response = await axios.get(url);
     
   this.incrementPage();
   // console.log(response);
